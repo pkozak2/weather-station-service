5021788b38c2215ee6209eba890242d3ed2c3ca9
Piotr Kozak <piotr.kozak@pirios.com>
2022-12-20 18:01:34 +0100

    httpclients

---
+++ WeatherStationService/Configuration/HttpClientsConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using WeatherStationService.HttpClients;
+
+namespace WeatherStationService.Configuration
+{
+    public static class HttpClientsConfiguration
+    {
+        public static void AddHttpClients(this WebApplicationBuilder builder, IConfiguration configuration)
+        {
+            var appSettings = new AppSettings();
+
+            configuration.GetSection("AppSettings").Bind(appSettings);
+
+            builder.Services.AddHttpClient<OpenMeteoClient>("OpenMeteoClient", x => { x.BaseAddress = new Uri("https://api.open-meteo.com/v1"); });
+            builder.Services.AddSingleton<IOpenMeteoClientFactory, OpenMeteoClientFactory>();
+        }
+    }
+}
---
+++ WeatherStationService/HttpClients/OpenMeteoClient.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Options;
+using WeatherStationService.Configuration;
+
+namespace WeatherStationService.HttpClients
+{
+    public interface IOpenMeteoClientFactory
+    {
+        public OpenMeteoClient GetClient();
+    }
+
+    public class OpenMeteoClientFactory : IOpenMeteoClientFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public OpenMeteoClientFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public OpenMeteoClient GetClient()
+        {
+            return _serviceProvider.GetRequiredService<OpenMeteoClient>();
+        }
+    }
+
+    public class OpenMeteoClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IOptionsMonitor<AppSettings> _options;
+
+        public OpenMeteoClient(HttpClient httpClient, IOptionsMonitor<AppSettings> options)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            _options = options;
+        }
+    }
+}
